3e7ec473fb2297cc8677e4476ab2b1c98853d1bf
mattstam <mattstam@live.com>
2024-07-18 10:52:47 -0700

    chore: rebase all succinct specific changes

--- lighthouse/docker-entrypoint.sh
+++ lighthouse/docker-entrypoint.sh
@@ -59,7 +59,7 @@ if [ -n "${RAPID_SYNC_URL}" ]; then
   echo "Checkpoint sync enabled"
   if [ "${ARCHIVE_NODE}" = "true" ]; then
     echo "Lighthouse archive node without pruning"
-    __prune="--reconstruct-historic-states --genesis-backfill"
+    __prune="--reconstruct-historic-states"
   else
     __prune=""
   fi
